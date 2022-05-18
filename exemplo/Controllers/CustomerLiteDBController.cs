24ec378ae155a50967390cb3e9463be831cd1028
Antonio Prestes <antonio_prestes@crgl-thirdparty.com>
2022-05-18 18:08:33 -0300

    commit projeto

---
+++ exemplo/Controllers/CustomerLiteDBController.cs
@@ -0,0 +1,51 @@
+﻿using exemplo.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerLiteDBController : ControllerBase
+    {
+        private readonly ILogger<CustomerLiteDBController> _logger;
+        private readonly string path;
+
+        public CustomerLiteDBController(ILogger<CustomerLiteDBController> logger)
+        {
+            _logger = logger;
+            path = Directory.GetCurrentDirectory()+ @"\MyData.db";
+            Console.WriteLine(path);
+        }
+
+        [HttpGet(Name = "GetCustomerLiteDB")]
+        public IEnumerable<Customer> Get()
+        {
+            using (var db = new LiteDatabase(path))
+            {
+                var col = db.GetCollection<Customer>("customers");
+
+                var results = col.Query()
+                    .ToList();
+                Console.WriteLine(results);
+                return results;
+            }
+        }
+
+        [HttpPost(Name = "PostCustomerLiteDB")]
+        public IEnumerable<Customer> Post(Customer customer)
+        {
+            using (var db = new LiteDatabase(path))
+            {
+                var col = db.GetCollection<Customer>("customers");
+
+                col.Insert(customer);
+                var results = col.Query()
+                    .Where(x=>x.Name == customer.Name)
+                    .ToList();
+                return results;
+            }
+        }
+    }
+}
---
+++ exemplo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
